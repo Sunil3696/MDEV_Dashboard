0577a6808aac05d09752f0674a469910500f74cf
sunil3696 <sunilstark3696@gmail.com>
2024-10-24 17:28:56 -0400

    added error showing

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -12,6 +12,7 @@ const Login = () => {
 
 let [data, setData] = useState(defaultData);
 let [error, setError] = useState(defaultData);
+const [loading,setLoading] = useState(false)
 
 
     const handleChange = (event) => {
@@ -50,26 +51,31 @@ let [error, setError] = useState(defaultData);
     }
 
 // console.log(error)
+const handleSubmit = (event) => {
+event.preventDefault();
+}
 
 
     return (
 <div className="login-container">
       <h2>Login</h2>
-      <form  className="login-form">
+      <form  className="login-form" onSubmit={handleSubmit}>
         <input
           type="email"
           placeholder="Email"
           name="email"
          onChange={handleChange}
         />
+        <p class="fieldError">{error?.email}</p>
         <input
           type="password"
           placeholder="Password"
           name="password"
           onChange={handleChange}
          />
+         <p class="fieldError">{error?.password}</p>
         <button type="submit">
-          Login
+        {loading ? "Logging in..." : "Login"}
         </button>
       </form>
       <p>
